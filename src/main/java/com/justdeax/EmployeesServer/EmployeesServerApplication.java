1df87cb61f339eb2f10f282b7f7a02b21a4d66c8
justdeax <adilmerimbaev@gmail.com>
2024-07-16 10:34:05 +0500

    v1.1

---
+++ src/main/java/com/justdeax/EmployeesServer/EmployeesServerApplication.java
@@ -0,0 +1,13 @@
+package com.justdeax.EmployeesServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justdeax/EmployeesServer/EmployeesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justdeax.EmployeesServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
